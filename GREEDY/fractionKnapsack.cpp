65c739d9e9a8a3c73c396aecfea58357fb4c2383
sanatan-dive <sanatansharma352@gmail.com>
2025-01-09 19:08:37 +0530

    greedy

--- GREEDY/fractionKnapsack.cpp
+++ GREEDY/fractionKnapsack.cpp
@@ -1,19 +1,29 @@
-class Solution {
-  public:
-    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
-        sort(val.begin(),val.end());
-        sort(wt.begin(),wt.end());
-        
-        int totVal = 0;
 
-      for(int i = 0; i < wt.size();i++){
-      if (wt[i]<=capacity){
-        totVal += wt[i];
-        capacity = capacity - wt[i];
-      }
-      else{
-        
-      }
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
+        vector<pair<double, int>> items;
+        for (int i = 0; i < val.size(); i++) 
+            items.push_back({(double)val[i] / wt[i], i});
+
+        sort(items.rbegin(), items.rend());
+
+        double totVal = 0;
+        for (auto& item : items) {
+            int idx = item.second;
+            if (wt[idx] <= capacity) {
+                totVal += val[idx];
+                capacity -= wt[idx];
+            } else {
+                totVal += item.first * capacity;
+                break;
+            }
+        }
+        return totVal;
     }
-  }
 };
